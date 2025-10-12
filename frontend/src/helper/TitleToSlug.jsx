a1470778bc1f30611579b2253cb1dc9216446a3f
Kaustav86x <kaustav86x@gmail.com>
2025-10-12 07:54:45 +0530

    12/10/25
    Helper list to map the blogs to its' contents and slug.

---
+++ frontend/src/helper/TitleToSlug.jsx
@@ -0,0 +1,9 @@
+import str from '../assets/StringsofMusic.md?raw';
+import cure from '../assets/TheCure.md?raw';
+import art from '../assets/ArtKills.md?raw';
+
+export const SlugBlogs = [
+  {title: 'StringsofMusic', content: str, slug: 'strings-of-music'},
+  {title: 'TheCure', content: cure, slug: 'the-cure'},
+  {title: 'ArtKills', content: art, slug: 'art-kills'}
+]
