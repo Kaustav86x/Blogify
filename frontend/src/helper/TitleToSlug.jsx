210b7dbb3ad20267bbd4dd914c4bbd787e8c53e1
Kaustav86x <kaustav86x@gmail.com>
2025-10-18 23:24:41 +0530

    18/10/25
    Content of Blogs, with addition of img property for future scope

--- frontend/src/helper/TitleToSlug.jsx
+++ frontend/src/helper/TitleToSlug.jsx
@@ -1,9 +1,17 @@
 import str from '../assets/StringsofMusic.md?raw';
 import cure from '../assets/TheCure.md?raw';
 import art from '../assets/ArtKills.md?raw';
+import unplg from '../assets/UnpluggedInAConcreteJungle.md?raw';
+import inv from '../assets/Invisible.md?raw';
+import crm from '../assets/CriminalPsychologyInMyWords.md?raw';
+import unname from '../assets/AnUnnamedPiece.md?raw';
 
 export const SlugBlogs = [
-  {title: 'StringsofMusic', content: str, slug: 'strings-of-music'},
-  {title: 'TheCure', content: cure, slug: 'the-cure'},
-  {title: 'ArtKills', content: art, slug: 'art-kills'}
+  {title: 'StringsofMusic', content: str, slug: 'strings-of-music', publishDate: new Date('2023-09-24'), image:''},
+  {title: 'TheCure', content: cure, slug: 'the-cure', publishDate: new Date('2025-01-18'), image:''},
+  {title: 'ArtKills', content: art, slug: 'art-kills', publishDate: new Date('2025-05-19'), image:''},
+  {title: 'UnpluggedInAConcreteJungle', content: unplg, slug: 'unplugged-in-a-concrete-jungle', publishDate: new Date('2025-07-20'), image:''},
+  {title: 'Invisible', content: inv, slug: 'invisible', publishDate: new Date('2025-05-20'), image:''},
+  {title: 'CriminalPsychologyInMyWords', content: crm, slug: 'criminal-psychology-in-my-words', publishDate: new Date('2024-05-12'), image:''},
+  {title: 'AnUnnamedPiece', content: unname, slug: 'an-unnamed-piece', publishDate: new Date('2025-10-05'), image:''},
 ]
