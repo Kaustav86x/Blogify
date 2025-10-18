b0f4c89ecd72da3c6d404bb91044422b7c21c803
Kaustav86x <kaustav86x@gmail.com>
2025-10-18 22:45:00 +0530

    18/10/25
    Content of the poem

---
+++ frontend/src/helper/Pieces.jsx
@@ -0,0 +1,15 @@
+import soldier from '../assets/ASoldierAndASoul.md?raw';
+import savior from '../assets/TheSavior.md?raw';
+import begin from '../assets/TheBeginning.md?raw';
+import birth from '../assets/Rebirth.md?raw';
+import voidd from '../assets/TheVoid.md?raw';
+
+export const SlugPieces = [
+
+{title: 'ASoldierAndASoul', content: soldier, slug: 'a-soldier-and-a-soul', publishDate: new Date('2023-08-15'), image:''},
+{title: 'TheSavior', content: savior, slug: 'the-savior', publishDate: new Date('2023-10-10'), image:''},
+{title: 'TheBeginning', content: begin, slug: 'the-beginning', publishDate: new Date('2024-02-14'), image:''},
+{title: 'Rebirth', content: birth, slug: 'rebirth', publishDate: new Date('2024-06-18'), image:''},
+{title: 'TheVoid', content: voidd, slug: 'the-void', publishDate: new Date('2024-11-22'), image:''}
+
+]
