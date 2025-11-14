38de727236604b3860316a56bc1595c9f589be0a
Kaustav86x <kaustav86x@gmail.com>
2025-11-14 17:36:47 +0530

    14/11/25
    MySection is fixed for mobile

--- frontend/src/components/MySection.jsx
+++ frontend/src/components/MySection.jsx
@@ -10,7 +10,7 @@ const MySection = () => {
   const aboutSectionRef = useRef(null);
 
   return (
-    <div className="flex flex-col md:flex-row w-full">
+    <div className="flex flex-col md:flex-row w-full p-2">
       {/* Left Image */}
       <div className="relative md:w-1/2 h-[400px] md:h-[666px] flex justify-center items-center bg-sky-300">
         <img className="w-2/3 max-w-sm h-auto object-cover" src={img5} alt="Kaustav" data-aos="fade-left" />
@@ -23,8 +23,20 @@ const MySection = () => {
         id="about"
         ref={aboutSectionRef}
       >
-        <h3 className="text-black text-3xl sm:text-4xl font-poor-story">Hi, I'm Kaustav</h3>
-        <p className="text-black text-lg sm:text-2xl font-poor-story">
+        {/* <div className="relative w-[100px] flex flex-col items-center justify-start pt-6 ml-15"> */}
+        {/* Horizontal Line */}
+        <div className=''>
+        <div className="w-[122px] h-0 left-0 top-0 absolute border-t border-black mb-6"/>
+
+        {/* Vertical Line */}
+
+        <div className='flex lg:flex-row'>
+        {/* <div className="w-px h-150 bg-black rotate-0" /> */}
+        {/* </div> */}
+
+        <div className='flex flex-col'>
+        <h3 className="text-black text-3xl sm:text-4xl font-poor-story pb-3">Hi, I'm Kaustav</h3>
+        <p className="text-black text-lg sm:text-2xl font-poor-story p-10">
           A software engineer by profession and blogger by passion. I write on complex human thoughts
           and random topics people find “weird” and “different”.
         </p>
@@ -33,6 +45,12 @@ const MySection = () => {
             Read More
           </button>
         </Link>
+        </div>
+        
+        </div>
+
+        </div>
+
       </div>
     </div>
   )
