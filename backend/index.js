2b1498c67db6cdebb54266785fb4db17c0173c6f
Kaustav <70485937+Kaustav86x@users.noreply.github.com>
2026-08-28 02:05:55 +0530

    28/08/26
    Fixed typos

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,8 @@ app.options("*", cors()); //handle preflight requests
 const allowedOrigins = [
     'https://blogify-frontend.vercel.app',
     'http://localhost:5173',
-    'https://www.kdey.blog/',
-    'https://kdey.blog/',           // without www as well
+    'https://www.kdey.blog',
+    'https://kdey.blog',           // without www as well
     "http://localhost:5173",          // keep for local dev
     "http://localhost:8080",
     'https://blogify-backend-ochre.vercel.app',
